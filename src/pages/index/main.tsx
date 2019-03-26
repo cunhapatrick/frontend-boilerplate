920ffc2843898d29a5859c9808990f899e503fb6
Patrick Cunha <patrick.cunha336@gmail.com>
2019-03-26 17:03:39 -0300

    update main page example with react hooks

--- src/pages/index/main.tsx
+++ src/pages/index/main.tsx
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "../../app/components/button";
 import "./styles.main.less";
-export default class extends Component {
-  render() {
-    return (
-      <div className="title">
-        <span className="example">Hello Conaz</span>
-        <Button />
-      </div>
-    );
-  }
+export default function main() {
+  return (
+    <div className="title">
+      <span className="example">Hello Conaz</span>
+      <Button />
+    </div>
+  );
 }
