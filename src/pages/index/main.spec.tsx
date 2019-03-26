b854fb79c5e30c5fab7f38c1617e43acd5ee428c
Patrick Cunha <patrick.cunha336@gmail.com>
2019-03-26 17:36:46 -0300

    update e2e test / additional note comments with instructions

--- src/pages/index/main.spec.tsx
+++ src/pages/index/main.spec.tsx
@@ -5,11 +5,14 @@ import Main from "./main";
 // import { shallow, mount } from "enzyme";
 // NOTE Faker is used to generate random values to e2e tests
 // import faker from "faker";
+// NOTE puppeteer is used to open and manipulate a browser to operate e2e tests
 import puppeteer from "puppeteer";
 
 describe("Example Test Page", () => {
   it("Should render a button", async () => {
     const browser = await puppeteer.launch({
+      // NOTE Change this part to not disable the display of the browser for better
+      // performance on the test environment (headless: false || true)
       headless: false
     });
     const page = await browser.newPage();
@@ -19,6 +22,7 @@ describe("Example Test Page", () => {
         width: 500,
         height: 2400
       },
+
       userAgent: ""
     });
 
